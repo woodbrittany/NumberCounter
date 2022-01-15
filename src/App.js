84f31a955efe978eb545f83e731388bebcc589ab
woodbrittany <woodbrittany3@gmail.com>
2022-01-15 13:47:38 -0600

    Number Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(isRed);
+};
+
+return(
+    <div className="app">
+    <h1 className={isRed ? "red" : ""}>Number Counter</h1>
+    <button onClick={increment}>Increment</button>
+    <h1>{count}</h1>
+ </div>
   );
 }
 
